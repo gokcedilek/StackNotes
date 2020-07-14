68c4674efc9e73a17cde23135cc4722f7820775d
gokcedilek <gokcedilek99@gmail.com>
2020-07-13 21:01:54 -0700

    switched to async await

--- content.js
+++ content.js
@@ -11,38 +11,70 @@ document.addEventListener('copy', (e) => {
   });
 });
 
-updateClipboard = (text) => {
-  navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+// updateClipboard = (text) => {
+//   navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+//     if (result.state == 'granted' || result.state == 'prompt') {
+//       navigator.clipboard
+//         .writeText(text)
+//         .then(() => {
+//           console.log('Copied to clipboard: ', text);
+//         })
+//         .catch((err) => {
+//           console.error('Could not copy text: ', err);
+//         });
+//     }
+//   });
+// };
+
+// readClipboard = () => {
+//   navigator.permissions.query({ name: 'clipboard-read' }).then((result) => {
+//     if (result.state == 'granted' || result.state == 'prompt') {
+//       navigator.clipboard
+//         .readText()
+//         .then((text) => {
+//           console.log('Read from the clipboard: ', text);
+//           //note that we should update the clipboard only AFTER we read from the clipboard - this problem happens as the tab asks for user confirmation to use the clipboard
+//           chrome.runtime.sendMessage({ message: 'paste' }, function (response) {
+//             updateClipboard(response.text);
+//           });
+//         })
+//         .catch((err) => {
+//           console.error('Could not read text: ', err);
+//         });
+//     }
+//   });
+// };
+
+const updateClipboard = async (text) => {
+  try {
+    const result = await navigator.permissions.query({
+      name: 'clipboard-write',
+    });
     if (result.state == 'granted' || result.state == 'prompt') {
-      navigator.clipboard
-        .writeText(text)
-        .then(() => {
-          console.log('Copied to clipboard: ', text);
-        })
-        .catch((err) => {
-          console.error('Could not copy text: ', err);
-        });
+      await navigator.clipboard.writeText(text);
+      console.log('Copied to clipboard: ', text);
     }
-  });
+  } catch (err) {
+    console.error('Could not copy text: ', err);
+  }
 };
 
-readClipboard = () => {
-  navigator.permissions.query({ name: 'clipboard-read' }).then((result) => {
+const readClipboard = async () => {
+  try {
+    const result = await navigator.permissions.query({
+      name: 'clipboard-read',
+    });
     if (result.state == 'granted' || result.state == 'prompt') {
-      navigator.clipboard
-        .readText()
-        .then((text) => {
-          console.log('Read from the clipboard: ', text);
-          //note that we should update the clipboard only AFTER we read from the clipboard - this problems happens as the tab asks for user confirmation to use the clipboard
-          chrome.runtime.sendMessage({ message: 'paste' }, function (response) {
-            updateClipboard(response.text);
-          });
-        })
-        .catch((err) => {
-          console.error('Could not read text: ', err);
-        });
+      const text = await navigator.clipboard.readText();
+      console.log('Read from the clipboard: ', text);
+      //note that we should update the clipboard only AFTER we read from the clipboard - this problem happens as the tab asks for user confirmation to use the clipboard
+      chrome.runtime.sendMessage({ message: 'paste' }, function (response) {
+        updateClipboard(response.text);
+      });
     }
-  });
+  } catch (err) {
+    console.error('Could not read text: ', err);
+  }
 };
 
 document.addEventListener('paste', function (e) {
